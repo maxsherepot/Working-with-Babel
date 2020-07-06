55fac4c9d86c4a69e693058c9a7b0ebe8a3f8cab
maxsherepot <48758609+maxsherepot@users.noreply.github.com>
2020-07-06 11:10:35 +0300

    installed babel preset-react for work with JSX

--- build/main.js
+++ build/main.js
@@ -1,16 +1,6 @@
-"use strict";
+import React from "react";
+import ReactDOM from "react-dom";
 
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+const App = () => /*#__PURE__*/React.createElement("p", null, "Hello");
 
-class App {
-  constructor() {
-    _defineProperty(this, "run", async (name = "World") => {
-      console.log(`Hello ${name}`);
-      console.log([1, 2, [2, 3]].flat());
-    });
-  }
-
-}
-
-const app = new App();
-app.run().then(() => console.log("done")).catch(() => console.log("Error"));
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.GetElementById("root"));
--- src/main.js
+++ src/main.js
@@ -1,13 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
 
-class App {
 
-    run = async (name = "World") => {
-        console.log(`Hello ${name}`);
-        console.log([1, 2, [2, 3]].flat());
-    };
-}
+const App = () => <p>Hello</p>;
 
-const app = new App();
-app.run()
-    .then(() => console.log("done"))
-    .catch(() => console.log("Error"));
+ReactDOM.render(<App />, document.GetElementById("root"));
